9f4c9665fbc0a2466fa27fa427601590e7f83e3f
bluegleam0101 <sennasoeditos2@gmail.com>
2022-02-01 21:17:45 +0100

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import flask
-
+#imports#
 
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+
+#flask app#
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'srg6789$*&@B#*BB*s$%^&*'
 
+#routes#
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+#start server code#
 
 if __name__ == "__main__":
     app.run(debug=True)
